621eb7c897275cd07099890bc630369cd78986f5
NoobPete <peteretep@live.dk>
2019-04-22 22:32:14 +0200

    Made it work steam wide (excluding checkout), more realiable, full path to image because it also works on the other steam domain

--- openinsteam.js
+++ openinsteam.js
@@ -2,10 +2,9 @@ var node = document.createElement("div");
 node.className = "header_installsteam_btn header_installsteam_btn_green"
 var link = document.createElement("a");
 link.className = "header_installsteam_btn_content";
-link.style = "background-color: #387097; background-position: 10px 2px; background-image: url( '/public/images/v6/icon_platform_win.png?v=3' );";
+link.style = "background-color: #387097; background-position: 10px 2px; background-image: url( 'https://steamstore-a.akamaihd.net/public/images/v6/icon_platform_win.png?v=3' );";
 link.innerHTML = "Open In Steam";
-var id = window.location.pathname.split("/")[2];
-link.href = "steam://advertise/" + id;
+link.href = "steam://openurl/" + window.location.href;
 node.appendChild(link);
 
 document.getElementById("global_action_menu").prepend(node);
